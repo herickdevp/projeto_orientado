958e9156f3a51441280ace6573699b548c772b48
Herick Silva <herick610@gmail.com>
2024-06-28 21:35:38 -0300

    Modal Login

--- FrontEnd_SysAlert/frontSysAlert/src/app/components/header-component/header-component.component.ts
+++ FrontEnd_SysAlert/frontSysAlert/src/app/components/header-component/header-component.component.ts
@@ -1,19 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, Input} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { RouterLink } from '@angular/router';
 import { RouterLinkActive } from '@angular/router';
+import { CommonModule, NgIf } from '@angular/common';
 
 
 @Component({
   selector: 'app-header-component',
   standalone: true,
-  imports: [RouterOutlet, RouterLink, RouterLinkActive],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive, NgIf],
   templateUrl: './header-component.component.html',
   styleUrl: './header-component.component.css'
 })
-export class HeaderComponentComponent {
-logo:string = 'logo.svg';
-IconeLogin:string = 'IconeLogin.svg';
-logoAlgar:string = 'Algar_Telecom_Logo.svg'
 
+export class HeaderComponentComponent {
+logo:string = 'logo.svg'; 
+IconeLogin:string = 'IconeLogin.svg';
+logoAlgar:string = 'Algar_Telecom_Logo.svg';
+logoLogin:string = 'logoLogin.svg';
+
+nivelUsuario:number = 2; // VARIVAVEL QUE VAI RECEBER NIVEL DO USUÁRIO DO BANCO DE DADOS
+
+// MODAL LOGIN - (Necessário importar o Input)
+isVisible = false;
+
+openModal() {
+  this.isVisible = true;
+}
+
+closeModal() {
+  this.isVisible = false;
+}
 }
